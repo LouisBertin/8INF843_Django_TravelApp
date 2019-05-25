3f97798e109778daed995de04ed3dea6cff37fed
LouisBertin <bertin.louis7@gmail.com>
2019-05-25 10:37:23 -0400

    create Post model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    departure = models.CharField(max_length=255)
+    arrival = models.CharField(max_length=255)
+    passengers_nb = models.IntegerField()
+    full = models.BooleanField()
+    pub_date = models.DateTimeField('date published')
