bb3c9ab6e33c8209790b7610549e51fb5016b85d
LouisBertin <bertin.louis7@gmail.com>
2019-06-05 15:19:34 -0400

    admin crud works

--- app/models.py
+++ app/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Post(models.Model):
     departure = models.CharField(max_length=255)
     arrival = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, null=True)
     passengers_nb = models.IntegerField()
     full = models.BooleanField()
     pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.title
