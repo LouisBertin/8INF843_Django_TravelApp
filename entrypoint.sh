2011c6a5b7970fe9d5cd27a650de3cc9356ce8ee
LouisBertin <bertin.louis7@gmail.com>
2019-06-05 19:00:37 -0400

    add bootstrap template

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 python manage.py migrate
-gunicorn project.wsgi:application --bind 0.0.0.0:8000 --workers 3
+gunicorn project.wsgi:application --bind 0.0.0.0:8000 --workers 3 --reload
