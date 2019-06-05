8426111a56a2ffc3a6973264c0707c92bca58e19
LouisBertin <bertin.louis7@gmail.com>
2019-06-05 19:29:26 -0400

    auth works

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
-
 from . import views
+from django.urls import include, path
+from app.registration.registration import SignUp
 
 urlpatterns = [
     path('', views.index, name='index'),
+    # auth
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('signup/', SignUp.as_view(), name='signup'),
 ]
